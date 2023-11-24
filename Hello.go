6c5d5f91dac84375035c9bf38b2b62df1a446ff2
BrianChen027 <brianking80227@gmail.com>
2023-11-24 12:03:02 -0500

    Add todo.go

--- Hello.go
+++
@@ -1,6 +0,0 @@
-// main.go file
-package main
-import "fmt"
-func main(){
- fmt.Println("Hello Go and Research Rocks, but testing 2")
-}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/BrianChen027/Go_TODO
+
+go 1.21.4
